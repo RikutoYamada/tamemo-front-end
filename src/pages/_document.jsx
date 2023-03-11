64e77112533e1053e025a15ad8c6596326505751
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-03-11 23:09:42 +0900

    Edit pages

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className='bg-gray-100'>
       <Head />
       <body>
         <Main />
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+import Button from '@/components/elements/Button/Button'
+import Card from '@/components/elements/Card/Card'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center mt-36'>
+      <Card>
+        <div className='flex justify-center'>
+          <h1>ログインしていません</h1>
+        </div>
+        <div className='flex justify-end mt-10'>
+          <Link href='/signin' passHref>
+            <Button>ログイン</Button>
+          </Link>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default Home
