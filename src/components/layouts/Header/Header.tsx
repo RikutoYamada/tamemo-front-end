efcc84b58db257e7d8fae34098a7922052f5ef5c
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-04-12 22:32:41 +0900

    Changed design to change depending on login status

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,27 +1,23 @@
-import { authenticationState } from '@/features/auth/stores/isAuthenticatedState'
 import Link from 'next/link'
-import { QueryClient, useQueryClient } from 'react-query'
-import { useRecoilValue } from 'recoil'
-import { parseCookies, setCookie } from 'nookies'
-import { useEffect, useState } from 'react'
-import { GetServerSideProps, NextPageContext } from 'next'
-import nookies from 'nookies'
-import { useSyncExternalStore } from 'react'
 
-export const Header = ({
-  isAuthenticated = false
+export type HeaderProps = {
+  currentUserId: string | null
 }
+export const Header = ({
+  currentUserId
+}: HeaderProps
 ) => {
+console.log(currentUserId)
   return (
     <header>
       <nav className='flex bg-teal-500 p-6 justify-between'>
         <Link href='/' passHref>
-          <span className='font-semibold text-xl text-white'>MyApp</span>
+          <span className='font-semibold text-xl text-white'>tamemo</span>
         </Link>
-        {isAuthenticated &&
+        {currentUserId &&
           <button className='text-teal-100 hover:text-white'>ログアウト</button>
         }
-        {!isAuthenticated &&
+        {!currentUserId &&
           <div className='flex w-36 justify-between'>
             <Link href='/signin' className='text-teal-100 hover:text-white'>ログイン</Link>
             <Link href='/signin' className='text-teal-100 hover:text-white'>新規登録</Link>
