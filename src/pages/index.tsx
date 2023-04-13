7a248291ffa0b3e5361f26b973a208475b17c77f
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-04-13 22:25:12 +0900

    renamed index.js -> index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Card from '@/components/elements/Card/Card'
 import cookies from "@/utils/cookies"
+import { NextPageContext } from 'next';
 
-export async function getServerSideProps(ctx) {
+export async function getServerSideProps(ctx: NextPageContext) {
   const currentUserId = cookies.get(ctx).current_user_id
   return {
     props: { currentUserId: currentUserId || null }
