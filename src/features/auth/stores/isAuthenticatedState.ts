d795d67996bfa1378ddde34e0bf5842fb1af68c2
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-04-14 22:59:25 +0900

    Delete isAuthenticatedState since it is not used

--- src/features/auth/stores/isAuthenticatedState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil'
-
-type authenticationStatus = 'authenticated' | 'notAuthenticated' | 'loading'
-export const authenticationState = atom<authenticationStatus>({
-  key: 'authenticationStatus',
-  default: 'notAuthenticated'
-})
