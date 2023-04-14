645d86c4367a4d4b0ea5411b00978243ee630418
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-04-14 22:28:07 +0900

    Add logout function

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useLogout } from '@/features/auth/hooks/useLogout'
 
 export type HeaderProps = {
   currentUserId: string | null
@@ -7,6 +8,8 @@ export const Header = ({
   currentUserId
 }: HeaderProps
 ) => {
+
+  const { data, error, isLoading, mutate } = useLogout()
   return (
     <header>
       <nav className='flex bg-teal-500 p-6 justify-between'>
@@ -14,7 +17,7 @@ export const Header = ({
           <span className='font-semibold text-xl text-white'>tamemo</span>
         </Link>
         {currentUserId &&
-          <button className='text-teal-100 hover:text-white'>ログアウト</button>
+          <button className='text-teal-100 hover:text-white' onClick={() => mutate()}>ログアウト</button>
         }
         {!currentUserId &&
           <div className='flex w-36 justify-between'>
