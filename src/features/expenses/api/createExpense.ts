1e9a41c1ec1e82735fe10ea8900948232e516044
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-06-06 22:56:42 +0900

    Add Expense type

--- src/features/expenses/api/createExpense.ts
+++ src/features/expenses/api/createExpense.ts
@@ -1,7 +1,14 @@
 import { axios } from "@/lib/axios"
 import cookies from "@/utils/cookies"
 
-export const createExpense = (data) => {
+export type Expense = {
+  subExpenseCategoryId: number
+  amount: number
+  store: string
+  expendedAt: string
+}
+
+export const createExpense = (expense: Expense) => {
   const userId = cookies.get().current_user_id
-  return axios.post(`/users/${userId}/expenses`, data)
+  return axios.post(`/users/${userId}/expenses`, expense)
 }
