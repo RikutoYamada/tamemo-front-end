164117eb17a3983bcacc80145853b19ad7f3d7c2
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2023-06-21 22:27:40 +0900

    Add settings for using materialUI with NextJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,40 @@
-import '@/styles/globals.css'
-import { Header } from '@/components/layouts/Header'
-import { AppProps } from 'next/app'
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../theme';
+import createEmotionCache from '../createEmotionCache';
+
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil'
 
+import { Header } from '@/components/layouts/Header'
+
 const queryClient = new QueryClient()
 
-const App = ({ Component, pageProps }: AppProps) => {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) => {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Header currentUserId={pageProps.currentUserId} />
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <CacheProvider value={emotionCache}>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header currentUserId={pageProps.currentUserId} />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </CacheProvider>
   )
 }
 
-export default App
+export default MyApp
