a2aa6a7ba890da544c80ea9c28dca9e990e652fb
RikutoYamada <rikutoyamada1992@gmail.com>
2023-08-15 23:09:30 +0900

    Changed to be able to control the number of records of expenses to be retrieved from api

--- src/features/expenses/api/getExpenses.ts
+++ src/features/expenses/api/getExpenses.ts
@@ -1,7 +1,12 @@
 import { axios } from '@/lib/axios'
 import cookies from '@/utils/cookies'
 
-export const getExpenses = () => {
+export const getExpenses = (maxRecords?: number) => {
   const userId = cookies.get().current_user_id
+
+  if (maxRecords) {
+    return axios.get(`/users/${userId}/expenses?max_records=${maxRecords}`)
+  }
+
   return axios.get(`/users/${userId}/expenses`)
 }
--- src/features/expenses/hooks/useGetExpenses.ts
+++ src/features/expenses/hooks/useGetExpenses.ts
@@ -1,6 +1,6 @@
 import { useQuery } from 'react-query'
 import { getExpenses } from '../api/getExpenses'
 
-export const useGetExpenses = () => {
-  return useQuery('expenses', getExpenses)
+export const useGetExpenses = (maxRecords?: number) => {
+  return useQuery('expenses', () => getExpenses(maxRecords))
 }
